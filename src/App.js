1c039e6a01ae44b2655757405a79b367c7bcc881
dianajyoo <diana.yooj@gmail.com>
2019-02-19 01:41:46 -0500

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 import Home from "./components/Home"
-import './App.css';
+import '../node_modules/semantic-ui/dist/semantic.min.css'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -18,4 +19,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { NavLink } from 'react-router-dom'
+
 import "../../styling/Menu.css"
 
 const Menu = (props) => {
 
   return (
     <div className="menu">
-      hi
+        <NavLink to="/about" className="navlink">about</NavLink>
+        <NavLink to="/projects" className="navlink">work</NavLink>
     </div>
   )
 
