9b9a8a71aaa2a5eeccff09008eea0eea683e3ad6
dianajyoo <diana.yooj@gmail.com>
2019-02-21 00:03:05 -0500

    installed react-redux

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,17 @@ import { NavLink } from 'react-router-dom'
 
 import "../styling/NavBar.css"
 
-const Menu = (props) => {
+const NavBar = (props) => {
 
   return (
-    <div className="menu">
-      <NavLink to="/about" className="navlink" id="about">about</NavLink>
-      <NavLink to="/projects" className="navlink" id="work">work</NavLink>
-      <NavLink to="/contact" className="navlink" id="contact">contact</NavLink>
+    <div className="navbar">
+      <NavLink to="/" className="navlink" id="home">DIANA J. YOO</NavLink>
+      <NavLink to="/about" className="navlink" id="about">ABOUT</NavLink>
+      <NavLink to="/projects" className="navlink" id="work">WORK</NavLink>
+      <NavLink to="/contact" className="navlink" id="contact">CONTACT</NavLink>
     </div>
   )
 
 }
 
-export default Menu
+export default NavBar
