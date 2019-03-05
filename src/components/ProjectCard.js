bfb1a8e4cafbfca27d5ece72165ff95f9a2c0846
dianajyoo <diana.yooj@gmail.com>
2019-03-04 23:48:22 -0500

    made overlay on hover

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,8 @@ import "../styling/ProjectCard.css"
 const ProjectCard = ({photo}) => {
     return (
         <div className="card">
-            <NavLink to="/work" className="navlink"><img src={photo.source} alt="screenshot" className="preview" /></NavLink> 
+            <NavLink to="/work" className="navlink"><img src={photo.source} alt="screenshot" className="preview" /></NavLink>
+            <p className="overlay">TITLE</p>
         </div>
     )
 }
