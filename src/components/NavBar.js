7d34feb4409c3124bc384d708ec20043c453f5c0
dianajyoo <diana.yooj@gmail.com>
2019-03-19 22:43:11 -0400

    made navlinks change color on active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
 
   return (
     <div className="navbar">
-      <NavLink to="/" className="navlink" id="home">DIANA J. YOO</NavLink>
+      <NavLink exact to="/" className="navlink" id="home">DIANA J. YOO</NavLink>
       <NavLink to="/about" className="navlink" id="about">ABOUT</NavLink>
       <NavLink to="/projects" className="navlink" id="work">WORK</NavLink>
       <NavLink to="/contact" className="navlink" id="contact">CONTACT</NavLink>
