d4c918bd915e1a8dd23b64c12339ce83ff605ef4
dianajyoo <diana.yooj@gmail.com>
2019-04-09 18:02:12 -0400

    made project card responesive

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,14 +4,14 @@ import "../styling/ProjectCard.css"
 const ProjectCard = ({ match, project }) => {
     return (
         project.id === parseInt(match.params.workId) ? 
-          <div className="project-show">
+          <div className="project-container">
             <div id="project-id">#{project.id}<span>.</span></div>
             <div className="details-container">
               <img src={`${project.gif.split(", ")[0]}`} alt={project.name} className="gif" />
 
-              {project.gif.split(", ")[1] ? <img src={`${project.gif.split(", ")[1]}`} alt={project.name} className="gif" /> : null}
+              {/* {project.gif.split(", ")[1] ? <img src={`${project.gif.split(", ")[1]}`} alt={project.name} className="gif" /> : null} */}
 
-              <div className="details">{project.summary} <br /><br /> Built With: {project.tools} <br /><br /> State: In Development</div>
+              <div className="summary">{project.summary} <br /><br /> Built With: {project.tools} <br /><br /> State: In Development</div>
             </div>
           </div>
           : <div></div>
