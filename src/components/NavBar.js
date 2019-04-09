ec5b7dc277141f33cba75c3fddbb33e7a72e88d7
dianajyoo <diana.yooj@gmail.com>
2019-04-09 18:24:47 -0400

    made mobile navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,35 @@ import React from "react"
 import { NavLink } from "react-router-dom"
 import "../styling/NavBar.css"
 
-const NavBar = () => {
+class NavBar extends React.Component {
 
-  return (
-    <div className="navbar">
-      <NavLink exact to="/" className="navlink" id="home">D. J. YOO</NavLink>
-      <NavLink to="/about" className="navlink" id="about">ABOUT</NavLink>
-      <NavLink to="/projects" className="navlink" id="work">WORK</NavLink>
-      <NavLink to="/contact" className="navlink" id="contact">CONTACT</NavLink>
-    </div>
-  )
+  state = {
+    isOpen: false
+  }
+
+  toggleMenu = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="navbar">
+          <NavLink exact to="/" className="navlink" id="home">D. J. YOO</NavLink>
+          <NavLink to="/about" className="navlink" id="about">ABOUT</NavLink>
+          <NavLink to="/projects" className="navlink" id="work">WORK</NavLink>
+          <NavLink to="/contact" className="navlink" id="contact">CONTACT</NavLink>
+        </div>
+  
+        <div className="mobile-navbar">
+          <NavLink exact to="/" className="navlink" id="home">D. J. YOO</NavLink>
+          <i class="big bars icon" onClick={() => this.toggleMenu()}></i>
+        </div>
+      </div>
+    )
+  }
 
 }
 
