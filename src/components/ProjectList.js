50180dc1ab4ac00a25113967c72c41ff3c6699fb
dianajyoo <diana.yooj@gmail.com>
2019-04-10 20:59:00 -0400

    added a loader

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,13 +1,32 @@
 import React from "react"
 import "../styling/ProjectList.css"
 
-const ProjectList = ({ renderPhotos }) => {
+class ProjectList extends React.Component {
+
+  render() {
+
+    const { renderPhotos } = this.props
+
+    if (renderPhotos.length) {
+      return (
+        <div className="grid">
+          <div className="grid-container">
+            <p>WORK<span>.</span></p>
+            {renderPhotos}
+          </div>
+        </div>
+      )
+    }
+
     return (
       <div className="grid-container">
-        <p>WORK<span>.</span></p>
-        {renderPhotos}
+        <div className="lds-grid">
+          <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div>
       </div>
     )
+  }
+    
 }
 
 export default ProjectList
